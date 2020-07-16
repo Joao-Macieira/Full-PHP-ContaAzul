60f5cad4c014b05c36ba2b95c806baf03fcf463c
João Paulo <jp.macieira@uol.com.br>
2020-07-16 08:46:47 -0300

    ead 1.0

---
+++ controllers/homeController.php
@@ -0,0 +1,23 @@
+<?php
+class homeController extends controller {
+
+    public function __construct(){
+       
+        $alunos  = new Alunos();
+        
+        if(!$alunos->isLogged()) {
+            
+            header("Location: ".BASE_URL."login");
+            
+		}
+
+    }
+
+    public function index(){
+       $dados = array();
+
+
+       
+       $this->loadTemplate('home', $dados);
+    }  
+}
